5256c092927377fd6fcba2b98cef4024df6ab7d8
Shopsim <109350345+Shopsim@users.noreply.github.com>
2022-07-15 20:25:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function getRandomItem(arr) {
+    const array = [1, 'hello', 5, 8];
+
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
+    document.getElementById("phrase").innerHTML = item;
     return item;
 }
