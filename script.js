332d7e36f6752c262a085b65544aebf71194b97a
shopsim <109350345+shopsim@users.noreply.github.com>
2022-07-24 14:22:16 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function getRandomItem(arr) {
-    const array = [1, 'hello', 5, 8];
-
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const item = arr[randomIndex];
-    document.getElementById("phrase").innerHTML = item;
-    return item;
-}
